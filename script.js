bf2f7dd1af1db3c1604774f0441a6293e1d39630
substancex <wjeanmarie@bklawtech.com>
2017-01-10 22:54:47 +0000

    Finished mad libs

---
+++ script.js
@@ -0,0 +1,15 @@
+var words = {
+    animals : "Leopards",
+    things1 : "idiots",
+    feeling : "confused",
+    profession : "adventurer",
+    clothing : "jumpsuits",
+    things2 : "chairs",
+    person : "James",
+    place: "Rainforest",
+    verb : "fighting",
+    food : "Waffles!"
+}
+
+var sentence = "Say " + words.food + " the photographer said as the camera flashed! " + words.person + " and I had gone to the " + words.place + " to get our photos taken today. The first photo we really wanted was a picture of us dressed as " + words.animals + " pretending to be an " + words.profession + ". When we saw the proofs of it, I was a bit " + words.feeling + " because it looked different than I had imagined it would. (I hadn’t imagined so many " + words.things2 + " behind us.) However, the second photo was exactly what I wanted. We both looked like " + words.things1 + " wearing " + words.clothing + " and " + words.verb + " , which was exactly what I had in mind!";
+console.log(sentence);
